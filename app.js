12d26c97a5c7f4a0ea96a3f7663d8ab41d3bed67
Amy Li <liamy1996@hotmail.com>
2016-02-20 19:39:01 -0500

    Calendar broke my heart.
    cry.

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
-require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html'], function (headerTpl, homeTpl, footerTpl) {
+require.config({
+	paths:{
+	    bootstrap: '../js/libs/bootstrap.min',
+	    datepicker:'../js/libs/bootstrap-datepicker',
+	    jquery: '../js/libs/jquery-1.12.0.min',
+    	underscore: '../js/libs/underscore-min',
+    	Backbone: '../js/libs/backbone-min',
+	    },
+
+	    shim: {
+	    	underscore:{
+            	exports:"_"
+       	 	},
+
+       		Backbone:{
+            	deps: ['underscore','jquery'],
+            	exports: "Backbone"
+       	 	},
+
+	        bootstrap: {
+	        	deps:['jquery'],
+	        	exports:"bootstrap"
+
+	        },
+
+	        "datepicker" : {
+	   			deps: ["bootstrap"],
+	    		exports: "$.fn.datepicker"
+			}
+
+	    }
+
+});
+
+require(['Backbone',
+         'underscore',
+         'jquery',
+		 'libs/text!header.html', 
+		 'libs/text!home.html', 
+		 'libs/text!footer.html',  
+		 'datepicker',
+         'bootstrap'
+         ], function (Backbone,_,$,headerTpl, homeTpl, footerTpl, dp, bootstrap) {
 	
 	var ApplicationRouter = Backbone.Router.extend({
 		routes: {
@@ -39,16 +81,18 @@ require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html'
 	})
 	HomeView = Backbone.View.extend({
 		el: "#content",
-		// template: "home.html",
+		template: "home.html",
 		template: homeTpl,
 		initialize: function() {
 
 		},
 		render: function() {
 			$(this.el).html(_.template(this.template));
+			$("#calender-container .calender").datepicker({
+   				multidate: true
+			});
 		}
 	});
-	
 	
 	app = new ApplicationRouter();
 	Backbone.history.start();	
